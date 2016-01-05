39301d0c68403a943c3fb823e2e772e529b1e7d0
Amber Crawford <ammcrawford@gmail.com>
2016-01-05 11:42:01 -0700

    Customer has testing through find_all *Still needs testing for random

--- test/controllers/api/v1/customers_controller_test.rb
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -39,4 +39,54 @@ class Api::V1::CustomersControllerTest < ActionController::TestCase
     get :show, format: :json, id: Customer.first.id
     assert_equal Customer.first.id, json_response["id"]
   end
+
+  test "#find responds to json" do
+    get :find, format: :json, id: Customer.first.id
+    assert_response :success
+  end
+
+  test "#find returns one record" do
+    get :find, format: :json, id: Customer.first.id
+    assert_kind_of Hash, json_response
+  end
+
+  test "#find returns the correct customer" do
+    get :find, format: :json, id: Customer.first.id
+    assert_equal Customer.first.id, json_response["id"]
+  end
+
+  test "#find returns the correct customer using first name" do
+    get :find, format: :json, first_name: Customer.first.first_name
+    assert_equal Customer.first.first_name, json_response["first_name"]
+  end
+
+  test "#find returns the correct customer using last name" do
+    get :find, format: :json, last_name: Customer.first.last_name
+    assert_equal Customer.first.last_name, json_response["last_name"]
+  end
+
+  test "#find returns the null if first name is not in database" do
+    get :find, format: :json, first_name: "Not in there."
+    assert_equal "null", response.body
+  end
+
+  test "#find all responds to json" do
+    get :find_all, format: :json, id: Customer.first.id
+    assert_response :success
+  end
+
+  test "#find all returns an array of records" do
+    get :find_all, format: :json, first_name: Customer.first.first_name
+    assert_kind_of Array, json_response
+  end
+
+  test "#find all returns the correct customers using last name" do
+    get :find_all, format: :json, last_name: Customer.first.last_name
+    assert_equal Customer.first.last_name, json_response[0]["last_name"]
+  end
+
+  test "#find all returns the null if first name is not in database" do
+    get :find_all, format: :json, first_name: "Not in there."
+    assert_equal 0, json_response.count
+  end
 end
